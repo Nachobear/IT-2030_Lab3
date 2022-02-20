bea5cac8252eda8b76b2cb7d1f9f9039fa1cbc5f
Nachobear <kainerob@gmail.com>
2022-02-20 00:01:22 -0500

    Add project files.

---
+++ Lab3/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Lab3.Models;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.DA = 0;
+            ViewBag.TL = 0;
+            return View();
+        }
+
+        public IActionResult Index(PriceQuotationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.DA = model.CalculateDiscountAmount();
+                ViewBag.TL = model.CalculateTotal();
+            }
+            else
+            {
+                ViewBag.DA = 0;
+                ViewBag.TL = 0;
+            }
+            return View(model);
+        }
+
+
+    }
+}
+
+
+
+
---
+++ Lab3/Models/PriceQuotationModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab3.Models
+{
+    public class PriceQuotationModel
+    {
+        [Required(ErrorMessage = "Please enter a subtotal.")]
+        [Range(.01, Double.PositiveInfinity, ErrorMessage = "Subtototal must be 1 cent or greater")]
+        public decimal? Subtotal { get; set; }
+
+        [Required(ErrorMessage = "Please enter a discount percent.")]
+        [Range(1, 100, ErrorMessage = "Discount percent must be between 1 and 100.")]
+        public decimal? DiscountPercent { get; set; }
+
+        public decimal? CalculateDiscountAmount()
+        {
+            decimal? discountAmount = 0;
+            discountAmount = (Subtotal * (DiscountPercent / 100));
+            return discountAmount;
+        }
+
+        public decimal? CalculateTotal()
+        {
+            decimal? total = 0;
+            total = Subtotal - CalculateDiscountAmount();
+            return total;
+
+        }
+
+    }
+}
+
+
+
+
